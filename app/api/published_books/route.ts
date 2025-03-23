f042d19b38e7427a667eb9391640fe72924cf2b1
BuiDuongKham <khambui2003@gmail.com>
2025-03-23 17:03:20 +0000

    ignore /api

--- app/api/published_books/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-  const response = await fetch(`${process.env.BACKEND_API}/published_books/`);
-  const result = await response.json();
-  return NextResponse.json(result);
-}

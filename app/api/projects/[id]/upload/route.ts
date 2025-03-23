cf8dbf7b590a6a617185a9bfe639b42a8e6abc5b
BuiDuongKham <khambui2003@gmail.com>
2025-03-23 12:19:38 +0000

    printting api

--- app/api/projects/[id]/upload/route.ts
+++ app/api/projects/[id]/upload/route.ts
@@ -1,23 +1,27 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  console.log("Before parsing form data");
   const data = await req.formData();
-  const file = data.get('file');
-  const accessToken = data.get('accessToken');
-  const id = data.get('projectId');
+  const file = data.get("file");
+  const accessToken = data.get("accessToken");
+  const id = data.get("projectId");
 
   const formDataToSend = new FormData();
   if (file) {
-    formDataToSend.append('files', file);
+    formDataToSend.append("files", file);
   }
 
-  const response = await fetch(`${process.env.BACKEND_API}/projects/${id}/upload_images`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
+  const response = await fetch(
+    `${process.env.BACKEND_API}/projects/${id}/upload_images`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method: "POST",
+      body: formDataToSend,
     },
-    method: 'POST',
-    body: formDataToSend,
-  });
+  );
   const result = await response.json();
   return NextResponse.json(result);
 }

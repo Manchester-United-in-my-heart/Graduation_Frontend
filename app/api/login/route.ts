118bc9d58cdb31e3109c94e0e524598b74c176dd
BuiDuongKham <khambui2003@gmail.com>
2025-03-23 14:53:49 +0000

    pass the server api round

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -8,6 +8,8 @@ export async function POST(req: Request) {
   const form = new FormData();
   form.append("username", token.username);
   form.append("password", token.password);
+  console.log("Before sending data");
+  console.log(`${process.env.BACKEND_API}/token`);
   try {
     const response = await fetch(`${process.env.BACKEND_API}/token`, {
       method: "POST",
